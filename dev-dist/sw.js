abed476bf4a89cf1635315297414dd068d9aa328
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2024-11-20 17:30:34 +0200

    fixes 2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fbab7q4hgmg"
+    "revision": "0.1bgs5ar9de"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     VitePWA({
-      registerType: 'autoUpdate',  // Automatically updates the PWA
+      registerType: 'autoUpdate',
       manifest: {
         name: 'Budget ZA',
         short_name: 'Budget ZA',
-        description: 'Make Budget Simple',
-        theme_color: '#ffffff',
+        description: 'South African Budget Tracker',
+        theme_color: '#3498db',
         icons: [
           {
             src: '/images/favicon-96x96.png',
@@ -24,13 +24,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'], 
-        clientsClaim: true,
-        skipWaiting: true
-      },
-      devOptions: {
-        enabled: true,
-        type: 'module'
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
       }
     })
   ]
